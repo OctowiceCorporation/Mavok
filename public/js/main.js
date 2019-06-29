5762c9b0850215ace9ec8a1135487fe6fcab24c1
Eugene Popov <zhenya1995q@gmail.com>
2019-06-29 15:44:59 +0300

    Added ProductService

--- public/js/main.js
+++ public/js/main.js
@@ -153,16 +153,19 @@ $(document).ready(function () {
             .done(function(json) {
                 let array = JSON.parse(json);
                 $('#mobile-search-result').html('');
-                if(array.length === 0){
-                    let submit = $('<a></a>').addClass('list-group-item list-group-item-action').html('Ничего не найдено');
-                    $('#mobile-search-result').append(submit);
-                }
-                else {
-                    array.forEach(function (product) {
-                        let href = $('<a></a>').addClass('list-group-item list-group-item-action').attr('href', '/product/' + product['slug']).html(product['name']);
-                        $('#mobile-search-result').append(href);
-                    });
-                }
+                $('#mobile-search-result').autocomplete({
+                    source: [ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby" ]
+                });
+                // if(array.length === 0){
+                //     let submit = $('<a></a>').addClass('list-group-item list-group-item-action').html('Ничего не найдено');
+                //     $('#mobile-search-result').append(submit);
+                // }
+                // else {
+                //     array.forEach(function (product) {
+                //         let href = $('<a></a>').addClass('list-group-item list-group-item-action').attr('href', '/product/' + product['slug']).html(product['name']).css('z-index', 3);
+                //         $('#mobile-search-result').append(href);
+                //     });
+                // }
             });
     });
 
@@ -182,19 +185,21 @@ $(document).ready(function () {
         })
             .done(function(json) {
                 let array = JSON.parse(json);
-                $('#desktop-result').html('');
-                if(array.length === 0){
-                    let submit = $('<a></a>').addClass('list-group-item list-group-item-action').css('background-color','#0e8ce4').css('background-color','#0e8ce4').css('color', 'white').html('Ничего не найдено');
-                    $('#desktop-result').append(submit);
-                }
-                else {
-                    array.forEach(function (product) {
-                        let href = $('<a></a>').addClass('list-group-item list-group-item-action').attr('href', '/product/' + product['slug']).html(product['name']);
-                        $('#desktop-result').append(href);
-                    });
-                    let submit = $('<a></a>').addClass('list-group-item list-group-item-action').attr('href', '/product_search?search_text=' + value).css('background-color', '#0e8ce4').css('color', 'white').html('Посмотреть все варианты');
-                    $('#desktop-result').append(submit);
-                }
+                $('#desktop-result').autocomplete({
+                    source: [ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby" ]
+                });
+                // if(array.length === 0){
+                //     let submit = $('<a></a>').addClass('list-group-item list-group-item-action').css('background-color','#0e8ce4').css('background-color','#0e8ce4').css('color', 'white').html('Ничего не найдено');
+                //     $('#desktop-result').append(submit);
+                // }
+                // else {
+                //     array.forEach(function (product) {
+                //         let href = $('<a></a>').addClass('list-group-item list-group-item-action').attr('href', '/product/' + product['slug']).html(product['name']);
+                //         $('#desktop-result').append(href);
+                //     });
+                //     let submit = $('<a></a>').addClass('list-group-item list-group-item-action').attr('href', '/product_search?search_text=' + value).css('background-color', '#0e8ce4').css('color', 'white').html('Посмотреть все варианты');
+                //     $('#desktop-result').append(submit);
+                // }
             });
     });
 
