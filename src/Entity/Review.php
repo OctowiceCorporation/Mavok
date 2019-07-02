f20265f6f2902631185bc3e901455b76321e5672
dzhezar <dzhezik@gmail.com>
2019-07-02 19:34:51 +0300

    added missed files

---
+++ src/Entity/Review.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ReviewRepository")
+ */
+class Review
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $review;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $pros;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $cons;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_visible;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getReview(): ?string
+    {
+        return $this->review;
+    }
+
+    public function setReview(string $review): self
+    {
+        $this->review = $review;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getPros(): ?string
+    {
+        return $this->pros;
+    }
+
+    public function setPros(?string $pros): self
+    {
+        $this->pros = $pros;
+
+        return $this;
+    }
+
+    public function getCons(): ?string
+    {
+        return $this->cons;
+    }
+
+    public function setCons(?string $cons): self
+    {
+        $this->cons = $cons;
+
+        return $this;
+    }
+
+    public function getIsVisible(): ?bool
+    {
+        return $this->is_visible;
+    }
+
+    public function setIsVisible(bool $is_visible): self
+    {
+        $this->is_visible = $is_visible;
+
+        return $this;
+    }
+}
